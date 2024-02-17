c957ecdfb9cc86f8a57fbfafa8773487f16a4c25
Yakiv <35277595+fixuberi@users.noreply.github.com>
2024-02-17 19:55:22 +0200

    Add workspace lib with graphql definitions, Setup graphql on backend app with schema first grapql approach (#2)
    Co-authored-by: Yakiv Zhavoronkov <76595595+jtechical@users.noreply.github.com>

--- apps/learning-graph-api/src/app/app.module.ts
+++ apps/learning-graph-api/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: [join(process.cwd(),'libs/graph-ql/src/lib/schema.graphql')],
+      definitions: {
+        path: join(process.cwd(),'libs/graph-ql/src/lib/graphql.types.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+  ],
 })
 export class AppModule {}
