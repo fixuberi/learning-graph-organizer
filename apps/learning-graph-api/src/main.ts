22a500db332894971e913f2a8b840bd575b87946
Yakiv Zhavoronkov <76595595+jtechical@users.noreply.github.com>
2024-02-18 21:58:13 +0200

    Enable cors for api

--- apps/learning-graph-api/src/main.ts
+++ apps/learning-graph-api/src/main.ts
@@ -12,6 +12,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: 'http://localhost:****',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
