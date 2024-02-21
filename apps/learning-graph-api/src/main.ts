d35d574ca294c16b5d79d65d733be19ca44427b8
Yakiv Zhavoronkov <76595595+jtechical@users.noreply.github.com>
2024-02-21 23:04:59 +0200

    Add graphql codegen, connect test query on frontend

--- apps/learning-graph-api/src/main.ts
+++ apps/learning-graph-api/src/main.ts
@@ -12,10 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors({
-    origin: 'http://localhost:****',
-    credentials: true,
-  });
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
--- apps/learning-graph/src/app/app.config.ts
+++ apps/learning-graph/src/app/app.config.ts
@@ -1,7 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { InMemoryCache } from '@apollo/client/cache';
+import { APOLLO_OPTIONS, Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+export function createApollo(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({ uri: environment.graphqlApiUrl }),
+    cache: new InMemoryCache(),
+  };
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+    Apollo,
+  ],
 };
